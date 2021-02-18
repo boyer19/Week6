42dcff19f3db43fc23e8670b334626513492f044
Matt <ok7140mj@go.minnstate.edu>
2021-02-18 13:07:03 -0600

    Vidoe 5/8

---
+++ maps.js
@@ -0,0 +1,5 @@
+let metroAreaCenterCoordinates = [44.96, -93.2]         // Lat/Long coords
+let zoomLevel = 9                                       // 1 = Whole world.  20 = city blocks
+
+let map = L.map('college-map').setView(metroAreaCenterCoordinates, zoomLevel)
+
